2a460d1de6bc719e765bd3667b44a62aa1b3d031
blowfishpro <blowfishpro@yahoo.com>
2016-04-28 20:33:28 -0700

    Prep for 1.1.2

--- B9PartSwitch/Properties/AssemblyInfo.cs
+++ B9PartSwitch/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("B9PartSwitch")]
 [assembly: AssemblyDescription("A part switching module for KSP")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("blowfish")]
 [assembly: AssemblyProduct("B9PartSwitch")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.2.0")]
 
 [assembly: KSPAssembly("B9PartSwitch", 1, 1)]
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This mod will not change anything by itself, but is designed to be used by other
 
 ## Requirements
 
-* KSP version 1.1 (build 1230) is the only supported KSP version
+* KSP version 1.1.1 (build 1250) is the only supported KSP version
 * [ModuleManager](http://forum.kerbalspaceprogram.com/index.php?showtopic=50533) is required.
 
 ## Installation
@@ -29,6 +29,11 @@ The source can be found at [Github](https://github.com/blowfishpro/B9PartSwitch)
 This plugin is distributed under [LGPL v3.0](http://www.gnu.org/licenses/lgpl-3.0.en.html)
 
 ## Changelog
+
+### 1.1.2
+
+* Remove FSmeshSwitch and InterstellarMeshSwitch from incompatible modules
+* Recompile against KSP 1.1.1
 
 ### 1.1.1
 
