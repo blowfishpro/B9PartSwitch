d50d621c8e0a289253b68066206898519d10a4ab
blowfishpro <joseph.wong@appfolio.com>
2016-05-19 19:42:22 -0700

    Attempt Github deployment on tags

--- build_script/script/make_zip.sh
+++ build_script/script/make_zip.sh
@@ -2,11 +2,9 @@
 
 mkdir -v release
 mkdir -v zip_release
-mkdir -v zip_release/bintray
 cp -v -R GameData release
 mkdir -p release/GameData/${PROJECT_NAME}/Plugins
 cp -v ${PROJECT_DIR}/bin/Release/${PROJECT_NAME}.dll release/GameData/${PROJECT_NAME}/Plugins
 cp -v README.md release
 cp -v LICENSE release
 (cd release && zip -v -r ../zip_release/release.zip *)
-cp -v zip_release/release.zip zip_release/bintray/${PROJECT_NAME}_${TRAVIS_BRANCH}_${TRAVIS_BUILD_NUMBER}_$(git describe --tags).zip
