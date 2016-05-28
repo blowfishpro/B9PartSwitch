4b2e0a303aef54e92627211c33bfbd8b57118013
blowfishpro <blowfishpro@yahoo.com>
2016-05-28 13:52:25 -0700

    Add basic test framework
    May replace it with something better at some point

---
+++ B9PartSwitch/Test/PartModuleTest.cs
@@ -0,0 +1,45 @@
+﻿#if DEBUG
+
+using UnityEngine;
+using KSP.Testing;
+
+namespace B9PartSwitch.Test
+{
+    public abstract class PartModuleTest<T> : UnitTest where T : PartModule
+    {
+        protected GameObject gameObject;
+        protected GameObject gameObject2;
+        protected Part part;
+        protected T module;
+
+        public override void TestStartUp()
+        {
+            base.TestStartUp();
+
+            gameObject = new GameObject("part");
+            gameObject2 = new GameObject("model");
+            gameObject2.transform.parent = gameObject.transform;
+            part = gameObject.AddComponent<Part>();
+            part.enabled = false;
+            module = AddPartModule<T>();
+        }
+
+        protected U AddPartModule<U>() where U : PartModule
+        {
+            U module = gameObject.AddComponent<U>();
+            module.enabled = false;
+            part.Modules.Add(module);
+            return module;
+        }
+
+        public override void TestTearDown()
+        {
+            base.TestTearDown();
+
+            UnityEngine.Object.Destroy(gameObject);
+            UnityEngine.Object.Destroy(gameObject2);
+        }
+    }
+}
+
+#endif
