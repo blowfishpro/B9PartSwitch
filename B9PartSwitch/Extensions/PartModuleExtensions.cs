49a189130530e5c5318651000f4892e85a58f77d
blowfishpro <blowfishpro@yahoo.com>
2016-05-28 13:56:20 -0700

    Add method to check loading pass
    partInfo is assigned after PartModules have been parsed, so this can be
    used to check whether you're loading for the first time or from a
    save/craft

---
+++ B9PartSwitch/Extensions/PartModuleExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace B9PartSwitch
+{
+    public static class PartModuleExtensions
+    {
+        // partInfo is assigned after modules are created and loaded
+        public static bool ParsedPrefab(this PartModule module) => module.part.partInfo != null;
+    }
+}
