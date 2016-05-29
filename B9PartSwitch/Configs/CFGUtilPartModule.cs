6416c03caa81ac2615618eea169fb231bd20aed5
blowfishpro <blowfishpro@yahoo.com>
2016-05-29 10:38:40 -0700

    Remove unnecessary using directives

--- B9PartSwitch/Configs/CFGUtilPartModule.cs
+++ B9PartSwitch/Configs/CFGUtilPartModule.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Reflection;
+﻿using System.Linq;
 using UnityEngine;
 
 namespace B9PartSwitch
--- B9PartSwitch/Configs/ConfigField.cs
+++ B9PartSwitch/Configs/ConfigField.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Reflection;
-using UnityEngine;
-using KSP;
 
 namespace B9PartSwitch
 {
