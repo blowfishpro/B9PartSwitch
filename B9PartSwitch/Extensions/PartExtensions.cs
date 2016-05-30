a3f73fd633d8d5bc1763c9a44ba3de4a2e09b54c
blowfishpro <blowfishpro@yahoo.com>
2016-05-29 18:13:36 -0700

    Adjust part/resource extensions a bit

--- B9PartSwitch/Extensions/PartExtensions.cs
+++ B9PartSwitch/Extensions/PartExtensions.cs
@@ -25,13 +25,60 @@ namespace B9PartSwitch
             return resource;
         }
 
+        // Amount < 0 signifies use existing amount if exists, or create with max amount
+        public static PartResource AddOrCreateResource(this Part part, PartResourceDefinition info, float maxAmount, float amount)
+        {
+            if (amount > maxAmount)
+            {
+                part.LogWarning($"Cannot add resource '{info.name}' with amount > maxAmount, will use maxAmount (amount = {amount}, maxAmount = {maxAmount})");
+                amount = maxAmount;
+            }
+
+            PartResource resource = part.Resources[info.name];
+
+            if (resource == null)
+            {
+                if (amount < 0f)
+                    amount = maxAmount;
+
+                resource = part.AddResource(info, maxAmount, amount);
+            }
+            else
+            {
+                resource.maxAmount = maxAmount;
+
+                if (amount >= 0f)
+                    resource.amount = amount;
+            }
+
+            return resource;
+        }
+
         public static void RemoveResource(this Part part, PartResource resource)
         {
             if (!ReferenceEquals(resource.gameObject, part.gameObject) || !part.Resources.list.Contains(resource))
-                part.LogError($"Cannot remove resource '{resource.resourceName}' because it was not found");
+            {
+                part.LogError($"Cannot remove resource '{resource.resourceName}' because it is not on the correct part");
+                return;
+            }
 
             part.Resources.list.Remove(resource);
             UnityEngine.Object.Destroy(resource);
+        }
+
+        public static void RemoveResource(this Part part, string name)
+        {
+            PartResource resource = part.Resources[name];
+
+            if (resource.IsNotNull())
+            {
+                part.Resources.list.Remove(resource);
+                UnityEngine.Object.Destroy(resource);
+            }
+            else
+            {
+                part.LogError($"Cannot remove resource '{resource.resourceName}' because it was not found");
+            }
         }
         
         public static float GetResourceCostMax(this Part part) => part.Resources.list.Sum(resource => (float)resource.maxAmount * resource.info.unitCost);
