a38920ec6057c770f37ee8d208572d717c381bc8
blowfishpro <joseph.wong@appfolio.com>
2016-09-20 22:17:29 -0700

    File no longer needed (old way of getting KSP version)

--- build_script/env/get_ksp_version.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'json'
-
-version_info = JSON.parse(File.read('GameData/B9PartSwitch/B9PartSwitch.version'))
-ksp_version = version_info['KSP_VERSION']
-
-puts "#{ksp_version['MAJOR']}.#{ksp_version['MINOR']}.#{ksp_version['PATCH']}"
