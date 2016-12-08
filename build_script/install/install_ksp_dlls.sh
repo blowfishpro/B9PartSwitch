2b8a9dcc10946c81f10c97fb8f5966e32f494a88
blowfishpro <blowfishpro@yahoo.com>
2016-12-08 01:09:30 -0800

    Reference KSP DLLs rather than moving them
    Easier to deal with this way, and supports multiple projects

--- build_script/install/install_ksp_dlls.sh
+++ build_script/install/install_ksp_dlls.sh
@@ -2,5 +2,3 @@
 
 wget --no-check-certificate --quiet "https://drive.google.com/uc?export=download&id=${GDRIVE_DLL_PACKAGE_ID}" -O KSP_DLLs.zip
 unzip -P ${DLL_PACKAGE_ZIP_PASSPHRASE} KSP_DLLs.zip
-mkdir -v -p ${PROJECT_DIR}/bin/Release/
-cp -v KSP_DLLs/${KSP_VERSION}/* ${PROJECT_DIR}/bin/Release/
