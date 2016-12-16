27a1831a251eb99d2326126a8d65c64b0e26fa27
blowfishpro <blowfishpro@yahoo.com>
2016-12-15 23:17:56 -0800

    Modify WrappedConfigNode to allow adding nodes
    It doesn't really need to impement GetEnumerator for collection
    initializers

--- B9PartSwitchTests/WrappedConfigNode.cs
+++ B9PartSwitchTests/WrappedConfigNode.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
-
+using System;
 namespace B9PartSwitchTests
 {
     internal class WrappedConfigNode : ConfigNode, IEnumerable
     {
         public void Add(string name, string value) => AddValue(name, value);
+        public void Add(string name, ConfigNode node) => AddNode(name, node);
 
-        public IEnumerator GetEnumerator() => values.GetEnumerator();
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
