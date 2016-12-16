21caa98692027983b7f9e989449fe55c5f2f655b
blowfishpro <blowfishpro@yahoo.com>
2016-12-16 00:09:58 -0800

    Add test and more functionality to TestConfigNode

--- B9PartSwitchTests/TestUtils/TestConfigNode.cs
+++ B9PartSwitchTests/TestUtils/TestConfigNode.cs
@@ -5,8 +5,12 @@ namespace B9PartSwitchTests.TestUtils
 {
     internal class TestConfigNode : ConfigNode, IEnumerable
     {
+        public TestConfigNode() : base() { }
+        public TestConfigNode(string name) : base(name) { }
+
         public void Add(string name, string value) => AddValue(name, value);
         public void Add(string name, ConfigNode node) => AddNode(name, node);
+        public void Add(ConfigNode node) => AddNode(node);
 
         public IEnumerator GetEnumerator()
         {
