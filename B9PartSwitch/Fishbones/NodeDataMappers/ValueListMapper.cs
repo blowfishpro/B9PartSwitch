daf04c91b8fa7597e5f53ce334e182fc8abf313c
blowfishpro <blowfishpro@yahoo.com>
2016-12-16 23:06:28 -0800

    Add ValueListMapper

---
+++ B9PartSwitch/Fishbones/NodeDataMappers/ValueListMapper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using B9PartSwitch.Fishbones.Parsers;
+
+namespace B9PartSwitch.Fishbones.NodeDataMappers
+{
+    public class ValueListMapper : INodeDataMapper
+    {
+        private string name;
+        private IValueParser parser;
+
+        private Type elementType;
+        private Type listType;
+
+        public ValueListMapper(string name, IValueParser parser)
+        {
+            name.ThrowIfNullArgument(nameof(name));
+            parser.ThrowIfNullArgument(nameof(parser));
+
+            this.name = name;
+            this.parser = parser;
+
+            elementType = parser.ParseType;
+            listType = typeof(List<>).MakeGenericType(elementType);
+        }
+
+        public bool Load(ConfigNode node, ref object fieldValue)
+        {
+            node.ThrowIfNullArgument(nameof(node));
+            fieldValue.EnsureArgumentType(listType, nameof(fieldValue));
+
+            string[] values = node.GetValues(name);
+            if (values.Length == 0) return false;
+
+            if (fieldValue.IsNull()) fieldValue = Activator.CreateInstance(listType);
+            IList list = (IList)fieldValue;
+            foreach (string value in values)
+            {
+                if (value.IsNull()) continue;
+                object parsedValue = parser.Parse(value);
+                if (parsedValue.IsNotNull()) list.Add(parsedValue);
+            }
+
+            return true;
+        }
+
+        public bool Save(ConfigNode node, ref object fieldValue)
+        {
+            node.ThrowIfNullArgument(nameof(node));
+            fieldValue.EnsureArgumentType(listType, nameof(fieldValue));
+
+            IList list = (IList)fieldValue;
+            if (list.IsNullOrEmpty()) return false;
+
+            foreach (object value in list)
+            {
+                if (value.IsNull()) continue;
+                string formattedValue = parser.Format(value);
+                if (formattedValue.IsNotNull()) node.AddValue(name, formattedValue);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ B9PartSwitchTests/Fishbones/NodeDataMappers/ValueListMapperTest.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using B9PartSwitch.Fishbones.NodeDataMappers;
+using B9PartSwitchTests.TestUtils;
+
+namespace B9PartSwitchTests.Fishbones.NodeDataMappers
+{
+    public class ValueListMapperTest
+    {
+        private ValueListMapper mapper = new ValueListMapper("someValue", Exemplars.ValueParser);
+
+        [Fact]
+        public void TestLoad()
+        {
+            List<string> list = new List<string>();
+            object value = list;
+
+            ConfigNode node = new TestConfigNode
+            {
+                { "someValue", "blah1" },
+                { "someValue", "blah2" },
+                { "someOtherValue", "blah3" },
+            };
+
+            Assert.True(mapper.Load(node, ref value));
+            Assert.Same(list, value);
+            Assert.Equal(2, list.Count);
+
+            Assert.Equal("!!blah1!!", list[0]);
+            Assert.Equal("!!blah2!!", list[1]);
+        }
+
+        [Fact]
+        public void TestLoad__Null()
+        {
+            object value = null;
+
+            ConfigNode node = new TestConfigNode
+            {
+                { "someValue", "blah1" },
+                { "someValue", "blah2" },
+                { "someOtherValue", "blah3" },
+            };
+
+            Assert.True(mapper.Load(node, ref value));
+            Assert.NotNull(value);
+            Assert.IsType<List<string>>(value);
+
+            List<string> list = (List<string>)value;
+
+            Assert.Equal(2, list.Count);
+
+            Assert.Equal("!!blah1!!", list[0]);
+            Assert.Equal("!!blah2!!", list[1]);
+        }
+
+        [Fact]
+        public void TestLoad__ExistingValue()
+        {
+            List<string> list = new List<string> { "blah0" };
+            object value = list;
+
+            ConfigNode node = new TestConfigNode
+            {
+                { "someValue", "blah1" },
+                { "someValue", "blah2" },
+                { "someOtherValue", "blah3" },
+            };
+
+            Assert.True(mapper.Load(node, ref value));
+            Assert.Same(list, value);
+            Assert.Equal(3, list.Count);
+
+            Assert.Equal("blah0", list[0]);
+            Assert.Equal("!!blah1!!", list[1]);
+            Assert.Equal("!!blah2!!", list[2]);
+        }
+
+        [Fact]
+        public void TestLoad__NoValues()
+        {
+            List<string> list = new List<string> { "blah0" };
+            object value = list;
+
+            ConfigNode node = new TestConfigNode
+            {
+                { "someOtherValue", "blah3" },
+            };
+
+            Assert.False(mapper.Load(node, ref value));
+            Assert.Same(list, value);
+            Assert.Equal(1, list.Count);
+
+            Assert.Equal("blah0", list[0]);
+        }
+
+        [Fact]
+        public void TestLoad__NullNode()
+        {
+            object dummy = new List<string> { "blah0" };
+            Assert.Throws<ArgumentNullException>(() => mapper.Load(null, ref dummy));
+        }
+
+        [Fact]
+        public void TestLoad__WrongType()
+        {
+            object dummy = "why are you passing a string?";
+
+            ConfigNode node = new TestConfigNode
+            {
+                { "someValue", "something" },
+            };
+
+            Assert.Throws<ArgumentException>(() => mapper.Load(new ConfigNode(), ref dummy));
+            Assert.Throws<ArgumentException>(() => mapper.Load(node, ref dummy));
+        }
+
+        [Fact]
+        public void TestSave()
+        {
+            List<string> list = new List<string> { "blah1", "blah2" };
+            object value = list;
+            ConfigNode node = new ConfigNode();
+
+            Assert.True(mapper.Save(node, ref value));
+            Assert.Equal(new[] { "$$blah1$$", "$$blah2$$" }, node.GetValues("someValue"));
+
+            Assert.Equal(new[] { "blah1", "blah2" }, list.ToArray());
+        }
+
+        [Fact]
+        public void TestSave__Null()
+        {
+            object value = null;
+            ConfigNode node = new ConfigNode();
+
+            Assert.False(mapper.Save(node, ref value));
+            Assert.Empty(node.GetValues("someValue"));
+            Assert.Null(value);
+        }
+
+        [Fact]
+        public void TestSave__Empty()
+        {
+            object value = new List<string>();
+            ConfigNode node = new TestConfigNode
+            {
+                { "someValue", "blah0" },
+            };
+
+            Assert.False(mapper.Save(node, ref value));
+            Assert.Equal(new[] { "blah0" }, node.GetValues("someValue"));
+            Assert.Empty((List<string>)value);
+        }
+
+        [Fact]
+        public void TestSave__ExistingValue()
+        {
+            List<string> list = new List<string> { "blah1", "blah2" };
+            object value = list;
+            ConfigNode node = new TestConfigNode
+            {
+                { "someValue", "blah0" },
+            };
+
+            Assert.True(mapper.Save(node, ref value));
+            Assert.Equal(new[] { "blah0", "$$blah1$$", "$$blah2$$" }, node.GetValues("someValue"));
+
+            Assert.Equal(new[] { "blah1", "blah2" }, list.ToArray());
+        }
+
+        [Fact]
+        public void TestSave__NullInList()
+        {
+            List<string> list = new List<string> { "blah1", null, "blah2" };
+            object value = list;
+            ConfigNode node = new ConfigNode();
+
+            Assert.True(mapper.Save(node, ref value));
+            Assert.Equal(new[] { "$$blah1$$", "$$blah2$$" }, node.GetValues("someValue"));
+        }
+
+        [Fact]
+        public void TestSave__NullNode()
+        {
+            object dummy = new List<string> { "blah0" };
+            Assert.Throws<ArgumentNullException>(() => mapper.Save(null, ref dummy));
+        }
+
+        [Fact]
+        public void TestSave__WrongType()
+        {
+            object dummy = "why are you passing a string?";
+
+            ConfigNode node = new TestConfigNode
+            {
+                { "someValue", "something" },
+            };
+
+            Assert.Throws<ArgumentException>(() => mapper.Save(new ConfigNode(), ref dummy));
+            Assert.Throws<ArgumentException>(() => mapper.Save(node, ref dummy));
+        }
+    }
+}
