9c7108072811578c80fd8430becbd07f28659817
blowfishpro <blowfishpro@yahoo.com>
2016-12-16 21:16:30 -0800

    Add NodeScalarMapper

---
+++ B9PartSwitch/Fishbones/NodeDataMappers/NodeScalarMapper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace B9PartSwitch.Fishbones.NodeDataMappers
+{
+    public class NodeScalarMapper : INodeDataMapper
+    {
+        private readonly string name;
+        private readonly Type fieldType;
+
+        public NodeScalarMapper(string name, Type fieldType)
+        {
+            name.ThrowIfNullArgument(nameof(name));
+            fieldType.ThrowIfNullArgument(nameof(fieldType));
+
+            this.name = name;
+            this.fieldType = fieldType;
+        }
+
+        public bool Load(ConfigNode node, ref object result)
+        {
+            node.ThrowIfNullArgument(nameof(node));
+            if (result.IsNotNull() && !(result is IConfigNode)) throw new ArgumentException($"{nameof(result)} must be of type '{nameof(IConfigNode)}' (got '{result.GetType()}')");
+
+            ConfigNode innerNode = node.GetNode(name);
+            if (innerNode.IsNull()) return false;
+
+            if (result.IsNull()) result = Activator.CreateInstance(fieldType);
+
+            ((IConfigNode)result).Load(innerNode);
+
+            return true;
+        }
+
+        public bool Save(ConfigNode node, ref object input)
+        {
+            node.ThrowIfNullArgument(nameof(node));
+            if (input.IsNotNull() && !(input is IConfigNode)) throw new ArgumentException($"{nameof(input)} must be of type '{nameof(IConfigNode)}' (got '{input.GetType().ToString()}')");
+
+            if (input.IsNull()) return false;
+
+            ConfigNode innerNode = new ConfigNode(name);
+            ((IConfigNode)input).Save(innerNode);
+
+            node.AddNode(innerNode);
+
+            return true;
+        }
+    }
+}
---
+++ B9PartSwitchTests/Fishbones/NodeDataMappers/NodeScalarMapperTest.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using Xunit;
+using B9PartSwitch.Fishbones.NodeDataMappers;
+using B9PartSwitchTests.TestUtils;
+
+namespace B9PartSwitchTests.Fishbones.NodeDataMappers
+{
+    public class NodeScalarMapperTest
+    {
+        private class NodeDummy : IConfigNode
+        {
+            public string value;
+
+            public void Load(ConfigNode node)
+            {
+                value = node.GetValue("value");
+            }
+
+            public void Save(ConfigNode node)
+            {
+                node.SetValue("value", value, true);
+            }
+        }
+
+        private NodeScalarMapper mapper = new NodeScalarMapper("SOME_NODE", typeof(NodeDummy));
+
+        [Fact]
+        public void TestNew__ThrowsIfNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new NodeScalarMapper(null, typeof(NodeDummy)));
+            Assert.Throws<ArgumentNullException>(() => new NodeScalarMapper("SOME_NODE", null));
+        }
+
+        [Fact]
+        public void TestLoad()
+        {
+            object dummy = new NodeDummy();
+            object dummyRef = dummy;
+
+            ConfigNode node = new TestConfigNode
+            {
+                new TestConfigNode("SOME_NODE")
+                {
+                    { "value", "something" },
+                },
+                new TestConfigNode("SOME_OTHER_NODE")
+                {
+                    { "value", "something else" },
+                },
+            };
+
+            Assert.True(mapper.Load(node, ref dummy));
+            Assert.ReferenceEquals(dummyRef, dummy);
+            Assert.Equal("something", ((NodeDummy)dummy).value);
+        }
+
+        [Fact]
+        public void TestLoad__Null()
+        {
+            object dummy = null;
+
+            ConfigNode node = new TestConfigNode
+            {
+                new TestConfigNode("SOME_NODE")
+                {
+                    { "value", "something" },
+                },
+                new TestConfigNode("SOME_OTHER_NODE")
+                {
+                    { "value", "something else" },
+                },
+            };
+
+            Assert.True(mapper.Load(node, ref dummy));
+            Assert.NotNull(dummy);
+            Assert.Equal("something", ((NodeDummy)dummy).value);
+        }
+
+        [Fact]
+        public void TestLoad__NoNode()
+        {
+            object dummy = new NodeDummy { value = "abc" };
+            object dummyRef = dummy;
+
+            ConfigNode node = new TestConfigNode
+            {
+                new TestConfigNode("NOT_THE_NODE_YOURE_LOOKING_FOR")
+                {
+                    { "value", "something" },
+                },
+                new TestConfigNode("SOME_OTHER_NODE")
+                {
+                    { "value", "something else" },
+                },
+            };
+
+            Assert.False(mapper.Load(node, ref dummy));
+            Assert.ReferenceEquals(dummyRef, dummy);
+            Assert.Equal("abc", ((NodeDummy)dummy).value);
+        }
+
+        [Fact]
+        public void TestLoad__NullNode()
+        {
+            object dummy = new NodeDummy() { value = "abc" };
+            Assert.Throws<ArgumentNullException>(() => mapper.Load(null, ref dummy));
+        }
+
+        [Fact]
+        public void TestLoad__WrongType()
+        {
+            object dummy = "why are you passing a string?";
+
+            ConfigNode node = new TestConfigNode
+            {
+                new TestConfigNode("SOME_NODE")
+                {
+                    { "value", "something" },
+                },
+            };
+
+            Assert.Throws<ArgumentException>(() => mapper.Load(new ConfigNode(), ref dummy));
+            Assert.Throws<ArgumentException>(() => mapper.Load(node, ref dummy));
+        }
+
+        [Fact]
+        public void TestSave()
+        {
+            ConfigNode node = new ConfigNode();
+            object dummy = new NodeDummy { value = "foo" };
+
+            Assert.True(mapper.Save(node, ref dummy));
+
+            ConfigNode innerNode = node.GetNode("SOME_NODE");
+            Assert.NotNull(innerNode);
+            Assert.Equal("foo", innerNode.GetValue("value"));
+
+            Assert.Equal("foo", ((NodeDummy)dummy).value);
+        }
+
+        [Fact]
+        public void TestSave__NullValue()
+        {
+            ConfigNode node = new ConfigNode();
+            object dummy = null;
+
+            Assert.False(mapper.Save(node, ref dummy));
+
+            Assert.Null(node.GetNode("SOME_NODE"));
+        }
+
+        [Fact]
+        public void TestSave__NullNode()
+        {
+            object dummy = new NodeDummy { value = "abc" };
+            Assert.Throws<ArgumentNullException>(() => mapper.Save(null, ref dummy));
+        }
+
+        [Fact]
+        public void TestSave__WrongType()
+        {
+            object dummy = "why are you passing a string?";
+
+            Assert.Throws<ArgumentException>(() => mapper.Save(new ConfigNode(), ref dummy));
+        }
+    }
+}
