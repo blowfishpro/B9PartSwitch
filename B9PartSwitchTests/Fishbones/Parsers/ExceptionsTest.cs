d5590c328c637981bac3de3aed0f5ce64fad077f
blowfishpro <blowfishpro@yahoo.com>
2016-12-17 23:48:45 -0800

    Add parser exceptions
    These will be used in the next couple of commits

---
+++ B9PartSwitchTests/Fishbones/Parsers/ExceptionsTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xunit;
+using B9PartSwitch.Fishbones.Parsers;
+
+namespace B9PartSwitchTests.Fishbones.Parsers
+{
+    public class ExceptionsTest
+    {
+        private class SomeClass { }
+
+        [Fact]
+        public void TestParseTypeNotRegisteredException()
+        {
+            Exception e = new ParseTypeNotRegisteredException(typeof(SomeClass));
+
+            Assert.Equal("Attempted to get the parser for type 'B9PartSwitchTests.Fishbones.Parsers.ExceptionsTest+SomeClass', but it has not been registered", e.Message);
+        }
+
+        [Fact]
+        public void TestParseTypeNotRegisteredException__Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ParseTypeNotRegisteredException(null));
+        }
+
+        [Fact]
+        public void TestParseTypeAlreadyRegisteredException()
+        {
+            Exception e = new ParseTypeAlreadyRegisteredException(typeof(SomeClass));
+
+            Assert.Equal("Attempted to register perser for type 'B9PartSwitchTests.Fishbones.Parsers.ExceptionsTest+SomeClass', but it has already been registered", e.Message);
+        }
+
+        [Fact]
+        public void TestParseTypeAlreadyRegisteredException__Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ParseTypeAlreadyRegisteredException(null));
+        }
+    }
+}
