0b1523bc2d27e30c5681876cd24c43015e7aa5ce
blowfishpro <blowfishpro@yahoo.com>
2016-12-17 23:54:28 -0800

    Add ValueParseMapWrapper
    This allows another IValueParseMap to be accessed without potentially
    exposing the ability to add to it

---
+++ B9PartSwitch/Fishbones/Parsers/ValueParseMapWrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace B9PartSwitch.Fishbones.Parsers
+{
+    public class ValueParseMapWrapper : IValueParseMap
+    {
+        private readonly IValueParseMap map;
+
+        public ValueParseMapWrapper(IValueParseMap map)
+        {
+            map.ThrowIfNullArgument(nameof(map));
+            this.map = map;
+        }
+
+        public IValueParser GetParser(Type parseType)
+        {
+            parseType.ThrowIfNullArgument(nameof(parseType));
+            return map.GetParser(parseType);
+        }
+    }
+}
---
+++ B9PartSwitchTests/Fishbones/Parsers/ValueParseMapWrapperTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xunit;
+using B9PartSwitch.Fishbones.Parsers;
+
+namespace B9PartSwitchTests.Fishbones.Parsers
+{
+    public class ValueParseMapWrapperTest
+    {
+        private static IValueParser parser = new ValueParser<string>(s => s, s => s);
+        private class DummyParseMap : IValueParseMap
+        {
+            public IValueParser GetParser(Type parseType) => parser;
+        }
+
+        [Fact]
+        public void TestNew__Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ValueParseMapWrapper(null));
+        }
+
+        [Fact]
+        public void TestGetParser()
+        {
+            DummyParseMap map = new DummyParseMap();
+            ValueParseMapWrapper wrapper = new ValueParseMapWrapper(map);
+
+            Assert.Same(parser, wrapper.GetParser(typeof(string)));
+        }
+
+        [Fact]
+        public void TestGet__Null()
+        {
+            DummyParseMap map = new DummyParseMap();
+            ValueParseMapWrapper wrapper = new ValueParseMapWrapper(map);
+
+            Assert.Throws<ArgumentNullException>(() => wrapper.GetParser(null));
+        }
+    }
+}
