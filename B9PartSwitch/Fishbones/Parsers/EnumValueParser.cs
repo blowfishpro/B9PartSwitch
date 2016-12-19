0270e6062d345e2ec594c46ff67f5733e80f4c79
blowfishpro <blowfishpro@yahoo.com>
2016-12-18 22:14:53 -0800

    Add EnumValueParser
    should be able to parse and format any enum

---
+++ B9PartSwitch/Fishbones/Parsers/EnumValueParser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace B9PartSwitch.Fishbones.Parsers
+{
+    public class EnumValueParser : IValueParser
+    {
+        private readonly Type enumType;
+
+        public EnumValueParser(Type enumType)
+        {
+            enumType.ThrowIfNullArgument(nameof(enumType));
+            if (!enumType.IsEnum) throw new ArgumentException($"Expecting enum type but got '{enumType}'", nameof(enumType));
+
+            this.enumType = enumType;
+        }
+
+        public object Parse(string value)
+        {
+            value.ThrowIfNullArgument(nameof(value));
+            return Enum.Parse(enumType, value);
+        }
+
+        public string Format(object value)
+        {
+            value.ThrowIfNullArgument(nameof(value));
+            value.EnsureArgumentType(enumType, nameof(value));
+
+            return Enum.Format(enumType, value, "g");
+        }
+
+        public Type ParseType => enumType;
+    }
+}
