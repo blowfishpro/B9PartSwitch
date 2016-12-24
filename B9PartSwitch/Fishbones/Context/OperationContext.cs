409fd96c715b4f65a1f26a60652ac7bee4212cc8
blowfish <blowfishpro@yahoo.com>
2016-12-23 22:13:44 -0800

    Add Context.OperationContext
    Combines object chain and Operation, so that you can tell exactly what
    object is being parsed, what ancestors exist in the parse chain, and
    what operation is being called

---
+++ B9PartSwitch/Fishbones/Context/OperationContext.cs
@@ -0,0 +1,31 @@
+﻿namespace B9PartSwitch.Fishbones.Context
+{
+    public class OperationContext
+    {
+        public readonly Operation Operation;
+        public readonly OperationContext ParentOperation;
+        public readonly object Subject;
+
+        public OperationContext(Operation operation, object subject)
+        {
+            subject.ThrowIfNullArgument(nameof(subject));
+
+            Operation = operation;
+            Subject = subject;
+        }
+
+        public OperationContext(OperationContext parentOperation, object subject)
+        {
+            parentOperation.ThrowIfNullArgument(nameof(parentOperation));
+            subject.ThrowIfNullArgument(nameof(subject));
+
+            ParentOperation = parentOperation;
+            Operation = ParentOperation.Operation;
+            Subject = subject;
+        }
+
+        public object Parent => ParentOperation?.Subject;
+
+        public object Root => ParentOperation?.Root ?? Subject;
+    }
+}
