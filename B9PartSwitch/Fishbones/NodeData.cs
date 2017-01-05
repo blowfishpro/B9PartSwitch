6d51c6bb6d2384f0cb38df848e99d7cbd35281e2
blowfish <blowfishpro@yahoo.com>
2017-01-04 23:31:52 -0800

    Add NodeData attribute
    Not used yet

---
+++ B9PartSwitch/Fishbones/NodeData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace B9PartSwitch.Fishbones
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class NodeData : Attribute
+    {
+        public string name;
+        public bool persistent = false;
+    }
+}
