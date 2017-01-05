a05da9f822b9c6932fa5035d3bba8798bc5d7b71
blowfish <blowfishpro@yahoo.com>
2017-01-04 22:42:44 -0800

    Add NodeDataField
    Hopefully this is the highest object in the dependency injection tree,
    which is getting rather deep at this point

---
+++ B9PartSwitch/Fishbones/TypeConfigs/NodeDataField.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using B9PartSwitch.Fishbones.FieldWrappers;
+using B9PartSwitch.Fishbones.NodeDataMappers;
+using B9PartSwitch.Fishbones.Context;
+
+namespace B9PartSwitch.Fishbones.TypeConfigs
+{
+    public class NodeDataField
+    {
+        private readonly IFieldWrapper field;
+        private readonly IOperaitonManager operationManager;
+
+        public NodeDataField(IFieldWrapper field, IOperaitonManager operationManager)
+        {
+            field.ThrowIfNullArgument(nameof(field));
+            operationManager.ThrowIfNullArgument(nameof(operationManager));
+
+            this.field = field;
+            this.operationManager = operationManager;
+        }
+
+        public void Load(object subject, ConfigNode node, OperationContext context)
+        {
+            subject.ThrowIfNullArgument(nameof(subject));
+            node.ThrowIfNullArgument(nameof(node));
+            context.ThrowIfNullArgument(nameof(context));
+
+            INodeDataMapper mapper = operationManager.MapperFor(context.Operation);
+            if (mapper.IsNull()) return;
+
+            object value = field.GetValue(subject);
+
+            if (mapper.Load(ref value, node, context))
+                field.SetValue(subject, value);
+        }
+
+        public void Save(object subject, ConfigNode node, OperationContext context)
+        {
+            subject.ThrowIfNullArgument(nameof(subject));
+            node.ThrowIfNullArgument(nameof(node));
+            context.ThrowIfNullArgument(nameof(context));
+
+            INodeDataMapper mapper = operationManager.MapperFor(context.Operation);
+            if (mapper.IsNull()) return;
+
+            object value = field.GetValue(subject);
+            mapper.Save(value, node, context);
+        }
+    }
+}
---
+++ B9PartSwitchTests/Fishbones/TypeConfigs/NodeDataFieldTest.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using Xunit;
+using NSubstitute;
+using B9PartSwitch.Fishbones.FieldWrappers;
+using B9PartSwitch.Fishbones.NodeDataMappers;
+using B9PartSwitch.Fishbones.TypeConfigs;
+using B9PartSwitch.Fishbones.Context;
+
+namespace B9PartSwitchTests.Fishbones.TypeConfigs
+{
+    public class NodeDataFieldTest
+    {
+        private IFieldWrapper fieldWrapper = Substitute.For<IFieldWrapper>();
+        private IOperaitonManager operationManager = Substitute.For<IOperaitonManager>();
+        private INodeDataMapper mapper = Substitute.For<INodeDataMapper>();
+
+        private NodeDataField field;
+
+        private object dummy = null;
+
+        public NodeDataFieldTest()
+        {
+            field = new NodeDataField(fieldWrapper, operationManager);
+        }
+
+        [Fact]
+        public void TestNew__NullArgument()
+        {
+            Assert.Throws<ArgumentNullException>(() => new NodeDataField(null, operationManager));
+            Assert.Throws<ArgumentNullException>(() => new NodeDataField(fieldWrapper, null));
+            Assert.Throws<ArgumentNullException>(() => new NodeDataField(null, null));
+        }
+
+        #region Load
+
+        [Fact]
+        public void TestLoad()
+        {
+            object subject = new object();
+            object value = new object();
+            ConfigNode node = new ConfigNode();
+            OperationContext context = new OperationContext(Operation.LoadPrefab, subject);
+
+            fieldWrapper.GetValue(subject).Returns(value);
+            operationManager.MapperFor(Operation.LoadPrefab).Returns(mapper);
+            mapper.Load(ref value, node, context).Returns(true);
+
+            field.Load(subject, node, context);
+
+            fieldWrapper.Received().GetValue(subject);
+            mapper.Received().Load(ref value, node, context);
+            fieldWrapper.Received().SetValue(subject, value);
+
+            mapper.DidNotReceiveWithAnyArgs().Save(null, null, null);
+        }
+
+        [Fact]
+        public void TestLoad__Null()
+        {
+            object subject = new object();
+            object value = new object();
+            ConfigNode node = new ConfigNode();
+            OperationContext context = new OperationContext(Operation.LoadPrefab, subject);
+
+            fieldWrapper.GetValue(subject).Returns(null);
+            operationManager.MapperFor(Operation.LoadPrefab).Returns(mapper);
+            mapper.Load(ref dummy, node, context).Returns(x =>
+            {
+                x[0] = value;
+                return true;
+            });
+
+            field.Load(subject, node, context);
+
+            fieldWrapper.Received().GetValue(subject);
+            mapper.Received().Load(ref value, node, context);
+            fieldWrapper.Received().SetValue(subject, value);
+
+            mapper.DidNotReceiveWithAnyArgs().Save(null, null, null);
+        }
+
+        [Fact]
+        public void TestLoad__False()
+        {
+            object subject = new object();
+            object value = new object();
+            ConfigNode node = new ConfigNode();
+            OperationContext context = new OperationContext(Operation.LoadPrefab, subject);
+
+            fieldWrapper.GetValue(subject).Returns(value);
+            operationManager.MapperFor(Operation.LoadPrefab).Returns(mapper);
+            mapper.Load(ref dummy, node, context).Returns(false);
+
+            field.Load(subject, node, context);
+
+            fieldWrapper.Received().GetValue(subject);
+            mapper.Received().Load(ref value, node, context);
+
+            fieldWrapper.DidNotReceiveWithAnyArgs().SetValue(null, null);
+            mapper.DidNotReceiveWithAnyArgs().Save(null, null, null);
+        }
+
+        [Fact]
+        public void TestLoad__NoMapper()
+        {
+            object subject = new object();
+            object value = new object();
+            ConfigNode node = new ConfigNode();
+            OperationContext context = new OperationContext(Operation.LoadPrefab, subject);
+
+            fieldWrapper.GetValue(subject).Returns(value);
+            operationManager.MapperFor(Operation.LoadPrefab).Returns((INodeDataMapper)null);
+
+            field.Load(subject, node, context);
+            
+            fieldWrapper.DidNotReceiveWithAnyArgs().GetValue(null);
+            fieldWrapper.DidNotReceiveWithAnyArgs().SetValue(null, null);
+        }
+
+        [Fact]
+        public void TestLoad__NullArgument()
+        {
+            object subject = new object();
+            ConfigNode node = new ConfigNode();
+            OperationContext context = new OperationContext(Operation.LoadPrefab, subject);
+
+            Assert.Throws<ArgumentNullException>(() => field.Load(null, node, context));
+            Assert.Throws<ArgumentNullException>(() => field.Load(subject, null, context));
+            Assert.Throws<ArgumentNullException>(() => field.Load(subject, node, null));
+            Assert.Throws<ArgumentNullException>(() => field.Load(null, null, null));
+        }
+
+        #endregion
+
+        #region Save
+
+        [Fact]
+        public void TestSave()
+        {
+            object subject = new object();
+            object value = new object();
+            ConfigNode node = new ConfigNode();
+            OperationContext context = new OperationContext(Operation.Save, subject);
+
+            fieldWrapper.GetValue(subject).Returns(value);
+            operationManager.MapperFor(Operation.Save).Returns(mapper);
+
+            field.Save(subject, node, context);
+
+            fieldWrapper.Received().GetValue(subject);
+            mapper.Received().Save(value, node, context);
+            fieldWrapper.DidNotReceiveWithAnyArgs().SetValue(null, null);
+
+            mapper.DidNotReceiveWithAnyArgs().Load(ref dummy, null, null);
+        }
+
+        [Fact]
+        public void TestSave__Null()
+        {
+            object subject = new object();
+            ConfigNode node = new ConfigNode();
+            OperationContext context = new OperationContext(Operation.Save, subject);
+
+            fieldWrapper.GetValue(subject).Returns(null);
+            operationManager.MapperFor(Operation.Save).Returns(mapper);
+
+            field.Save(subject, node, context);
+
+            fieldWrapper.Received().GetValue(subject);
+            mapper.Received().Save(null, node, context);
+            fieldWrapper.DidNotReceiveWithAnyArgs().SetValue(null, null);
+
+            mapper.DidNotReceiveWithAnyArgs().Load(ref dummy, null, null);
+        }
+
+        [Fact]
+        public void TestSave__NoMapper()
+        {
+            object subject = new object();
+            object value = new object();
+            ConfigNode node = new ConfigNode();
+            OperationContext context = new OperationContext(Operation.Save, subject);
+
+            fieldWrapper.GetValue(subject).Returns(value);
+            operationManager.MapperFor(Operation.Save).Returns((INodeDataMapper)null);
+
+            field.Save(subject, node, context);
+
+            fieldWrapper.DidNotReceiveWithAnyArgs().GetValue(null);
+            mapper.DidNotReceiveWithAnyArgs().Save(null, null, null);
+            fieldWrapper.DidNotReceiveWithAnyArgs().SetValue(null, null);
+
+            mapper.DidNotReceiveWithAnyArgs().Load(ref dummy, null, null);
+        }
+
+        [Fact]
+        public void TestSave__NullArgument()
+        {
+            object subject = new object();
+            ConfigNode node = new ConfigNode();
+            OperationContext context = new OperationContext(Operation.LoadPrefab, subject);
+
+            Assert.Throws<ArgumentNullException>(() => field.Save(null, node, context));
+            Assert.Throws<ArgumentNullException>(() => field.Save(subject, null, context));
+            Assert.Throws<ArgumentNullException>(() => field.Save(subject, node, null));
+            Assert.Throws<ArgumentNullException>(() => field.Save(null, null, null));
+        }
+
+        #endregion
+    }
+}
