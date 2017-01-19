8b31885c19c6c21b84828e9e7154f7ea5924ca65
blowfish <blowfishpro@yahoo.com>
2017-01-18 21:32:46 -0800

    ModuleB9DisableTransform
    It just disables transforms.  Does not support dynamic transforms as it removes itself after the first OnLoad

---
+++ B9PartSwitch/ModuleB9DisableTransform.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace B9PartSwitch
+{
+    public class ModuleB9DisableTransform : PartModule
+    {
+        public override void OnLoad(ConfigNode node)
+        {
+            base.OnLoad(node);
+
+            foreach (string transformName in node.GetValues("transform"))
+            {
+                Transform[] transforms = part.FindModelTransforms(transformName);
+
+                if (transforms.Length == 0)
+                {
+                    this.LogError($"No transforms named '{transformName}' found in model");
+                    continue;
+                }
+
+                foreach (Transform transform in transforms)
+                {
+                    transform.gameObject.SetActive(false);
+                }
+            }
+
+            part.RemoveModule(this);
+        }
+    }
+}
