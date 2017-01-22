9a40d1643dc4f6e22291c7092e6a0ad9bbde2fb9
blowfish <blowfishpro@yahoo.com>
2017-01-21 21:35:56 -0800

    Make name explicitly null

--- B9PartSwitch/Fishbones/NodeData.cs
+++ B9PartSwitch/Fishbones/NodeData.cs
@@ -5,7 +5,7 @@ namespace B9PartSwitch.Fishbones
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
     public class NodeData : Attribute
     {
-        public string name;
+        public string name = null;
         public bool persistent = false;
     }
 }
