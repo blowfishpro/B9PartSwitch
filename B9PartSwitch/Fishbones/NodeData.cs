28d008eab020046a78a2d3a28c73a32d31ea87df
blowfish <blowfishpro@yahoo.com>
2017-01-22 00:42:26 -0800

    NodeData should have AllowMultiple = false

--- B9PartSwitch/Fishbones/NodeData.cs
+++ B9PartSwitch/Fishbones/NodeData.cs
@@ -2,10 +2,11 @@
 
 namespace B9PartSwitch.Fishbones
 {
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false)]
     public class NodeData : Attribute
     {
         public string name = null;
         public bool persistent = false;
+        public bool alwaysSerialize = false;
     }
 }
