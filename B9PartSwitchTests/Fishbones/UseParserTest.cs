ddf11c798e933b7e1fa3da1a72c0c73c3f706fbd
blowfish <blowfishpro@yahoo.com>
2017-01-22 12:17:27 -0800

    Add UseParser attribute
    Allows additional parsers to be considered for a particular field
    without creating a completely custom value parser map

---
+++ B9PartSwitchTests/Fishbones/UseParserTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Xunit;
+using B9PartSwitch.Fishbones;
+using B9PartSwitch.Fishbones.Parsers;
+using B9PartSwitchTests.TestUtils.DummyTypes;
+
+namespace B9PartSwitchTests.Fishbones
+{
+    public class UseParserTest
+    {
+        private class DummyValueParser : IValueParser
+        {
+            public Type ParseType { get { throw new NotImplementedException(); } }
+
+            public string Format(object value)
+            {
+                throw new NotImplementedException();
+            }
+
+            public object Parse(string value)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private class DummyValueParserWithNoConstructor : DummyValueParser
+        {
+            private DummyValueParserWithNoConstructor() { }
+        }
+
+        [Fact]
+        public void TestNew()
+        {
+            UseParser useParser = new UseParser(typeof(DummyValueParser));
+        }
+
+        [Fact]
+        public void TestNew__NullArgument()
+        {
+            Assert.Throws<ArgumentNullException>(() => new UseParser(null));
+        }
+
+        [Fact]
+        public void TestNew__BadType()
+        {
+            Assert.Throws<ArgumentException>(() => new UseParser(typeof(DummyClass)));
+        }
+
+        [Fact]
+        public void TestNew__NoParameterlessConstructor()
+        {
+            Assert.Throws<ArgumentException>(() => new UseParser(typeof(DummyValueParserWithNoConstructor)));
+        }
+
+        [Fact]
+        public void TestCreateParser()
+        {
+            UseParser useParser = new UseParser(typeof(DummyValueParser));
+
+            Assert.IsType<DummyValueParser>(useParser.CreateParser());
+        }
+    }
+}
