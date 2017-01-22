863b42e81de8642b783dbdf88d9f91cd10a40fc9
blowfish <blowfishpro@yahoo.com>
2017-01-21 23:42:36 -0800

    Inline IOperationManager
    It's only implemented by one thing

--- B9PartSwitch/Fishbones/OperationManager.cs
+++ B9PartSwitch/Fishbones/OperationManager.cs
@@ -3,6 +3,11 @@ using B9PartSwitch.Fishbones.NodeDataMappers;
 
 namespace B9PartSwitch.Fishbones
 {
+    public interface IOperaitonManager
+    {
+        INodeDataMapper MapperFor(Operation operation);
+    }
+
     public class OperationManager : IOperaitonManager
     {
         public readonly INodeDataMapper parseMapper;
