8e6475d611b5374b8d81c989c2ca63edb1e322a7
blowfish <blowfishpro@yahoo.com>
2017-01-22 20:56:33 -0800

    NodeDataListLibrary
    Manages single instances of each NodeDataList to avoid having to
    re-create them repeatedly

---
+++ B9PartSwitch/Fishbones/NodeDataListLibrary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace B9PartSwitch.Fishbones
+{
+    public static class NodeDataListLibrary
+    {
+        private static Dictionary<Type, NodeDataList> dict = new Dictionary<Type, NodeDataList>();
+
+        public static NodeDataList Get<T>() => Get(typeof(T));
+
+        public static NodeDataList Get(Type type)
+        {
+            type.ThrowIfNullArgument(nameof(type));
+
+            NodeDataList list;
+
+            if (dict.TryGetValue(type, out list))
+                return list;
+
+            try
+            {
+                NodeDataListBuilder builder = new NodeDataListBuilder(type);
+                list = builder.CreateList();
+            }
+            catch(Exception e)
+            {
+                Debug.LogError($"Exception while generating field configuration for type {type}");
+                Debug.LogError("This is fatal");
+                Debug.LogError("Exception:");
+                Debug.LogError(e.ToString());
+                Debug.LogError("The game will now exit");
+                Application.Quit();
+            }
+
+            dict[type] = list;
+            return list;
+        }
+    }
+}
