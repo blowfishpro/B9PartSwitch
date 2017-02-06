7f774e7cf7d1cb13b76394aa817c7b32d927071e
blowfish <joeydwong@gmail.com>
2017-02-05 19:59:32 -0800

    Put readme and license in GameData/B9PartSwitch
    Makes redistribution easier

--- build_script/script/make_zip.sh
+++ build_script/script/make_zip.sh
@@ -3,8 +3,10 @@
 mkdir -v release
 mkdir -v zip_release
 cp -v -R GameData release
-mkdir -p release/GameData/${PROJECT_NAME}/Plugins
-cp -v ${PROJECT_DIR}/bin/Release/${PROJECT_NAME}.dll release/GameData/${PROJECT_NAME}/Plugins
+mkdir -p "release/GameData/${PROJECT_NAME}/Plugins"
+cp -v "${PROJECT_DIR}/bin/Release/${PROJECT_NAME}.dll" "release/GameData/${PROJECT_NAME}/Plugins/"
 cp -v README.md release
 cp -v LICENSE release
+cp -v README.md "release/GameData/${PROJECT_NAME}"
+cp -v LICENSE "release/GameData/${PROJECT_NAME}"
 (cd release && zip -v -r ../zip_release/release.zip *)
