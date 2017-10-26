77553a7f2ff20282bf0292f1da680fa9424ad090
blowfish <blowfishpro@yahoo.com>
2017-10-25 22:22:11 -0700

    remove bintray

--- build_script/before_deploy/copy_release.sh
+++ build_script/before_deploy/copy_release.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env sh
 
-mkdir -v deploy/bintray
-cp -v zip_release/release.zip deploy/bintray/${PROJECT_NAME}_${TRAVIS_BRANCH}_${TRAVIS_BUILD_NUMBER}_$(git describe --tags).zip
-
 mkdir -v deploy/s3
 cp -v zip_release/release.zip deploy/s3/${PROJECT_NAME}_${TRAVIS_BRANCH}_${TRAVIS_BUILD_NUMBER}_$(git describe --tags).zip
 
