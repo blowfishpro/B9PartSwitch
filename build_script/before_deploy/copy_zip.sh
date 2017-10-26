ef924f469f6083dbb1864fc9ffeeeb59de53aaaf
blowfish <blowfishpro@yahoo.com>
2017-10-25 20:49:44 -0700

    try to deploy to s3

--- build_script/before_deploy/copy_zip.sh
+++ build_script/before_deploy/copy_zip.sh
@@ -3,6 +3,9 @@
 mkdir -v zip_release/bintray
 cp -v zip_release/release.zip zip_release/bintray/${PROJECT_NAME}_${TRAVIS_BRANCH}_${TRAVIS_BUILD_NUMBER}_$(git describe --tags).zip
 
+mkdir -v zip_release/s3
+cp -v zip_release/release.zip zip_release/s3/${PROJECT_NAME}_${TRAVIS_BRANCH}_${TRAVIS_BUILD_NUMBER}_$(git describe --tags).zip
+
 if [ ${TRAVIS_TAG} ]; then
   mkdir -v zip_release/github
   cp -v zip_release/release.zip zip_release/github/${PROJECT_NAME}_${TRAVIS_TAG}.zip
