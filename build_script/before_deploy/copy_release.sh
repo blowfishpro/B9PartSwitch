ee9fa6072942aad0c12ed43fd6e9817c9cb0e321
blowfish <blowfishpro@yahoo.com>
2017-10-25 21:56:59 -0700

    put deployment files in deploy/ instead of zip_release/

--- build_script/before_deploy/copy_release.sh
+++ build_script/before_deploy/copy_release.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
-mkdir -v zip_release/bintray
-cp -v zip_release/release.zip zip_release/bintray/${PROJECT_NAME}_${TRAVIS_BRANCH}_${TRAVIS_BUILD_NUMBER}_$(git describe --tags).zip
+mkdir -v deploy/bintray
+cp -v zip_release/release.zip deploy/bintray/${PROJECT_NAME}_${TRAVIS_BRANCH}_${TRAVIS_BUILD_NUMBER}_$(git describe --tags).zip
 
-mkdir -v zip_release/s3
-cp -v zip_release/release.zip zip_release/s3/${PROJECT_NAME}_${TRAVIS_BRANCH}_${TRAVIS_BUILD_NUMBER}_$(git describe --tags).zip
+mkdir -v deploy/s3
+cp -v zip_release/release.zip deploy/s3/${PROJECT_NAME}_${TRAVIS_BRANCH}_${TRAVIS_BUILD_NUMBER}_$(git describe --tags).zip
 
 if [ ${TRAVIS_TAG} ]; then
-  mkdir -v zip_release/github
-  cp -v zip_release/release.zip zip_release/github/${PROJECT_NAME}_${TRAVIS_TAG}.zip
+  mkdir -v deploy/github
+  cp -v zip_release/release.zip deploy/github/${PROJECT_NAME}_${TRAVIS_TAG}.zip
 fi
