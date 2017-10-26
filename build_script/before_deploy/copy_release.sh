9c21186aa3241f7ee751892aefd27757ecb296db
blowfish <blowfishpro@yahoo.com>
2017-10-25 22:10:07 -0700

    move avc file from bintray to s3

--- build_script/before_deploy/copy_release.sh
+++ build_script/before_deploy/copy_release.sh
@@ -9,4 +9,7 @@ cp -v zip_release/release.zip deploy/s3/${PROJECT_NAME}_${TRAVIS_BRANCH}_${TRAVI
 if [ ${TRAVIS_TAG} ]; then
   mkdir -v deploy/github
   cp -v zip_release/release.zip deploy/github/${PROJECT_NAME}_${TRAVIS_TAG}.zip
+
+  mkdir -v deploy/avc
+  cp -v release/GameData/B9PartSwitch/B9PartSwitch.version deploy/avc/B9PartSwitch.version
 fi
