ccc20e82f45783cf7ee5af1d8bbdfbafaa5fcfe1
blowfish <blowfishpro@yahoo.com>
2017-10-27 12:55:30 -0700

    fill AssemblyInfo.cs via erb

--- build_script/script/assembly_info.rb
+++ build_script/script/assembly_info.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 
+require 'erubis'
+
 require_relative '../version/tag_version'
+require_relative '../version/ksp_version'
 
 project_name = ARGV[0]
 
@@ -12,26 +15,17 @@ version = tag_version
 
 puts "Generating AssemblyInfo for project '#{project_name}' with version #{version.full_version}"
 
-attributes_data = {
-  'AssemblyVersion' 	  => ["\"#{version.full_version}\""],
-  'AssemblyFileVersion' => ["\"#{version.full_version}\""],
-  'KSPAssembly'		   	  => ["\"#{project_name}\"", version.major, version.minor],
-}
-
-attributes = attributes_data.map { |key, val| "[assembly: #{key}(#{val.join(', ')})]" }
-
-assembly_info_in = File.read('files/AssemblyInfo.cs.erb')
-
-assembly_info_elements = [assembly_info_in] + attributes + [nil]
-
-assembly_info_out = assembly_info_elements.join("\n")
-
 assembly_info_dir = File.join(project_name, 'Properties')
 assembly_info_file = File.join(assembly_info_dir, 'AssemblyInfo.cs')
 
 Dir.mkdir(assembly_info_dir) unless Dir.exist? assembly_info_dir
 
+erb = Erubis::Eruby.new(File.read('files/AssemblyInfo.cs.erb'))
+
 puts "Writing AssemblyInfo to '#{assembly_info_file}'"
-File.open(assembly_info_file, 'w+') { |f| f.write(assembly_info_out) }
+
+File.open(assembly_info_file, 'w+') do |f|
+  f.write erb.result(project_name: project_name, version: version, ksp_version: ksp_version)
+end
 
 puts 'Done generating AssemblyInfo'
