54978c65b52c63889b84d15e8329284cbf630289
blowfish <blowfishpro@yahoo.com>
2017-10-31 18:20:22 -0700

    make avc_version.rb more generic

--- build_script/script/avc_version.rb
+++ build_script/script/avc_version.rb
@@ -6,7 +6,7 @@ require_relative '../version/tag_version'
 require_relative '../version/ksp_version'
 
 if ARGV.size != 2
-  puts "usage: #{__FILE__} in_erb_file out_version_file"
+  puts "usage: #{__FILE__} in_erb_file out_file"
   exit 1
 end
 
@@ -17,10 +17,10 @@ puts "#{__FILE__}: ksp version: #{ksp_version}"
 puts "#{__FILE__}: version: #{version}"
 
 in_erb_file = ARGV[0]
-out_version_file = ARGV[1]
+out_file = ARGV[1]
 
 erb = Erubis::Eruby.new(File.read(in_erb_file))
 
-File.open(out_version_file, 'w+') do |f|
+File.open(out_file, 'w+') do |f|
   f.write erb.result(version: version, ksp_version: ksp_version)
 end
