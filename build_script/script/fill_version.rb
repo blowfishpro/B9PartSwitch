bdeb4d7e6a811fbf69e5df57b7a479cd7ec0f873
blowfish <blowfishpro@yahoo.com>
2017-10-31 18:27:51 -0700

    add addtional logging to fill_version.rb

--- build_script/script/fill_version.rb
+++ build_script/script/fill_version.rb
@@ -10,14 +10,15 @@ if ARGV.size != 2
   exit 1
 end
 
+in_erb_file = ARGV[0]
+out_file = ARGV[1]
+
 version = tag_version
 
 puts "#{__FILE__}: filling version info into ERB template"
 puts "#{__FILE__}: ksp version: #{ksp_version}"
 puts "#{__FILE__}: version: #{version}"
-
-in_erb_file = ARGV[0]
-out_file = ARGV[1]
+puts "#{__FILE__}: '#{in_erb_file}' -> '#{out_file}'"
 
 erb = Erubis::Eruby.new(File.read(in_erb_file))
 
