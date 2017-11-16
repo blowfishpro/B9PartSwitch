5effbc2f7b3d0fcdbb33971c51d8204f9c61ea90
blowfish <joeydwong@gmail.com>
2017-11-15 21:44:53 -0800

    Allow dialog to hold multiple errors
    Having more than one dialog on the screen at once doesn't work very
    well.

--- B9PartSwitch/FatalErrorHandler.cs
+++ B9PartSwitch/FatalErrorHandler.cs
@@ -1,19 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace B9PartSwitch
 {
     public static class FatalErrorHandler
     {
+        private const int MAX_MESSAGE_COUNT = 10;
+        private static PopupDialog dialog;
+        private static List<string> allMessages = new List<string>();
+
         public static void HandleFatalError(string message)
         {
             try
             {
-                PopupDialog.SpawnPopupDialog(new Vector2(0.5f, 0.5f),
+                if (allMessages.Count < MAX_MESSAGE_COUNT && !allMessages.Contains(message))
+                {
+                    allMessages.Add(message);
+                }
+                else if (allMessages.Count == MAX_MESSAGE_COUNT)
+                {
+                    Debug.LogError("[FatalExceptionHandler] Not displaying fatal error because too many errors have already been added:");
+                    Debug.LogError(message);
+                    allMessages.Add("(too many error messages to display)");
+                }
+                else
+                {
+                    Debug.LogError("[FatalExceptionHandler] Not displaying fatal error because too many errors have already been added:");
+                    Debug.LogError(message);
+                    return;
+                }
+
+                if (dialog != null) dialog.Dismiss();
+
+                dialog = PopupDialog.SpawnPopupDialog(new Vector2(0.5f, 0.5f),
                     new Vector2(0.5f, 0.5f),
                     new MultiOptionDialog(
-                        "B9PartSwitchFatalError",
-                        $"B9PartSwitch has encountered a fatal error and KSP needs to close.\n\n{message}\n\nPlease see KSP's log for addtional details",
+                        "B9PartSwitchFatalError: " + message,
+                        $"B9PartSwitch has encountered a fatal error and KSP needs to close.\n\n{string.Join("\n\n", allMessages.ToArray())}\n\nPlease see KSP's log for addtional details",
                         "B9PartSwitch - Fatal Error",
                         HighLogic.UISkin,
                         new Rect(0.5f, 0.5f, 500f, 60f),
