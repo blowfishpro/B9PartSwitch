3d92d27781f0ffb0229c9a9972a07554f04f82ca
blowfish <blowfishpro@yahoo.com>
2018-10-17 10:44:09 -0700

    add test to ensure .version file is valid

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+group :development, :test do
+  gem 'bundler', '~> 1.16'
+  gem 'rake', '~> 12.3'
+  gem 'rest-client', '~> 2.0'
+  gem 'rspec', '~> 3.8'
+  gem 'rubocop', '~> 0.59.2'
+end
---
+++ spec/version_file_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+require 'json'
+require 'rest-client'
+
+RSpec.describe 'B9PartSwitch.version' do
+  it 'exists, is valid json, and is formatted correctly' do
+    expect(File.file?('release/GameData/B9PartSwitch/B9PartSwitch.version')).to be(true)
+
+    raw_contents = File.read('release/GameData/B9PartSwitch/B9PartSwitch.version')
+
+    parsed_json = JSON.parse(raw_contents)
+
+    name = parsed_json['NAME']
+    download = parsed_json['DOWNLOAD']
+    url = parsed_json['URL']
+    github = parsed_json['GITHUB']
+    version = parsed_json['VERSION']
+    ksp_version = parsed_json['KSP_VERSION']
+
+    expect(name).not_to be_nil
+    expect(download).not_to be_nil
+    expect(url).not_to be_nil
+
+    expect(RestClient.head(download).code).to be_between(200, 299)
+    expect(RestClient.head(url).code).to be_between(200, 299)
+
+    expect(github).not_to be_nil
+
+    github_username = github['USERNAME']
+    github_repository = github['REPOSITORY']
+
+    expect(github_username).not_to be_nil
+    expect(github_repository).not_to be_nil
+
+    expect(RestClient.head("https://github.com/#{github_username}/#{github_repository}").code).to be_between(200, 299)
+
+    expect(version).to match(/\d+(?:\.\d+)*/)
+    expect(ksp_version).to match(/\d+(?:\.\d+)*/)
+  end
+end
