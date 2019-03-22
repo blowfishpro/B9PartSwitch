3ffa6e4a55f7be7e964c809829109020216b4f8d
blowfish <joeydwong@gmail.com>
2019-03-21 21:59:38 -0700

    download KSP DLLs from S3

--- build_script/install/install_ksp_dlls.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-wget --no-check-certificate --quiet "https://drive.google.com/uc?export=download&id=${GDRIVE_DLL_PACKAGE_ID}" -O KSP_DLLs.zip
-unzip -P ${DLL_PACKAGE_ZIP_PASSPHRASE} KSP_DLLs.zip
