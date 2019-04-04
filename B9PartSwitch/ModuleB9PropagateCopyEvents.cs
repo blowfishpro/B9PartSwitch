462ce87221d8ab2f9fb484ca58fb5f6785cf0e3e
blowfish <joeydwong@gmail.com>
2019-04-03 22:23:14 -0700

    Add back empty ModuleB9PropagateCopyEvents
    Prevents a warning from being shown to the user when loading a craft.  Can be removed in a while once craft have been updated.

---
+++ B9PartSwitch/ModuleB9PropagateCopyEvents.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace B9PartSwitch
+{
+    // Leave an empty class here to allow craft files and saves to be loaded for a while without warnings, can eventually be removed
+    public class ModuleB9PropagateCopyEvents : PartModule { }
+}
