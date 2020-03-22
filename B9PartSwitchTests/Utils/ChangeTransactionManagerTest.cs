09ab174b99c077b3d4445f2c94e93428488d3513
blowfish <blowfishpro@users.noreply.github.com>
2020-03-22 14:13:55 -0700

    Add a missing test case

--- B9PartSwitchTests/Utils/ChangeTransactionManagerTest.cs
+++ B9PartSwitchTests/Utils/ChangeTransactionManagerTest.cs
@@ -81,6 +81,27 @@ namespace B9PartSwitchTests.Utils
         }
 
         [Fact]
+        public void TestWithTransaction__Throws()
+        {
+            int counter = 0;
+            ChangeTransactionManager transactionManager = new ChangeTransactionManager(() => counter += 1);
+            Exception ex1 = new Exception();
+            Exception ex2 = Assert.Throws<Exception>(delegate
+            {
+                transactionManager.WithTransaction(delegate
+                {
+                    throw ex1;
+                });
+            });
+
+            Assert.Equal(0, counter);
+            Assert.Same(ex1, ex2);
+
+            transactionManager.RequestChange();
+            Assert.Equal(1, counter);
+        }
+
+        [Fact]
         public void TestWithTransaction__Null()
         {
             ChangeTransactionManager transactionManager = new ChangeTransactionManager(() => throw new Exception());
