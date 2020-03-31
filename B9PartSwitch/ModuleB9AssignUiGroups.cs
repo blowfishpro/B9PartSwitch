fb694a95b1832d5c8dfd0d94122bc54ff026bdec
blowfish <blowfishpro@users.noreply.github.com>
2020-03-31 00:21:38 -0700

    Module for assigning UI groups on other modules
    called ModuleB9AssignUiGroups
    
    takes one or more `MODULE` nodes that each identify a module to have its UI group assigned
    * each one must have an `IDENTIFIER` node to identify the module
      * it must have a `name` which is the name of the module (wildcards and regex are allowed)
      * it can have any other fields that uniquely identify the module
      * This is the same as the `IDENTIFIER` in a module switcher
    * `uiGroupName` - unique identifier of the group
    * `uiGroupDisplayName` - human readable name of the group to show in the UI
    * only applies to fields/events that don't already have a group
    * Cannot apply to `ModuleB9PartSwitch`, `ModuleB9PartInfo`,
      `ModuleB9AssignUiGroups` (itself), or `ModuleSimpleAdjustableFairing`

---
+++ B9PartSwitch/ModuleB9AssignUiGroups.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using B9PartSwitch.Fishbones;
+using B9PartSwitch.Fishbones.Context;
+
+namespace B9PartSwitch
+{
+    public class ModuleB9AssignUiGroups : CustomPartModule
+    {
+        private static readonly string[] MODULE_NAME_BLACKLIST = { "ModuleSimpleAdjustableFairing" };
+        private static readonly Type[] MODULE_TYPE_BLACKLIST = { typeof(ModuleB9PartSwitch), typeof(ModuleB9PartInfo), typeof(ModuleB9AssignUiGroups) };
+
+        public class ModuleInfo : IContextualNode
+        {
+            [NodeData(name = "IDENTIFIER")]
+            public ConfigNode identifierNode;
+
+            [NodeData]
+            public string uiGroupName;
+
+            [NodeData]
+            public string uiGroupDisplayName;
+
+            public void Load(ConfigNode node, OperationContext context) => this.LoadFields(node, context);
+
+            public void Save(ConfigNode node, OperationContext context) => this.SaveFields(node, context);
+
+            public void Apply(Part part)
+            {
+                part.ThrowIfNullArgument(nameof(part));
+                if (identifierNode == null) throw new InvalidOperationException("Identifier node is null!");
+
+                ModuleMatcher moduleMatcher = new ModuleMatcher(identifierNode);
+                PartModule module = moduleMatcher.FindModule(part);
+
+                if (MODULE_TYPE_BLACKLIST.Any(type => module.GetType().Implements(type))) throw new Exception($"Cannot assign UI groups on {module}");
+                if (MODULE_NAME_BLACKLIST.Any(typeName => module.GetType().Name == typeName)) throw new Exception($"Cannot assign UI groups on {module}");
+
+                module.SetUiGroups(uiGroupName, uiGroupDisplayName);
+            }
+        }
+
+        [NodeData(name = "MODULE", alwaysSerialize = true)]
+        public List<ModuleInfo> moduleInfos = new List<ModuleInfo>();
+
+        public override void OnStart(StartState state)
+        {
+            base.OnStart(state);
+
+            if (moduleInfos.Count == 0) LogError("No MODULEs were specified");
+
+            foreach (ModuleInfo moduleInfo in moduleInfos)
+            {
+                try
+                {
+                    moduleInfo.Apply(part);
+                }
+                catch (Exception ex)
+                {
+                    LogError("Exception when setting up UI group for MODULE");
+                    Debug.LogException(ex);
+                }
+            }
+        }
+    }
+}
